f78e4978cea957ec68697b6f9219141e3ce49c64
Ryo Iida <iidazz0109@gmail.com>
2020-07-31 06:49:32 +0900

    json placeholderのURLに変更

--- docs/js/get_fetch.js
+++ docs/js/get_fetch.js
@@ -1,7 +1,7 @@
 const fetchForm = document.querySelector('.fetchForm');
 const btn = document.querySelector('.btn');
 const name = document.querySelector('.name');
-const url = 'https://my-json-server.typicode.com/ryo-i/testDB/data';
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
 const getFetch = () => {
     let formData = new FormData(fetchForm);
